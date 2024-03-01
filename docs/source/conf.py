7a35553169ea1de217001ba0e1a20ba852c06197
HughTB <hugh.baldwin77@gmail.com>
2024-03-01 13:17:28 +0000

    remove random files; set config.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'C20 Academy'
+copyright = '2024, SETaP Group 5A 2023'
+author = 'SETaP Group 5A 2023'
 
 release = '0.1'
 version = '0.1.0'
