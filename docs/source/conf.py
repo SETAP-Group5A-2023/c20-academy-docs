c47af1b4a265f7a5b4e707fcef641915a6ffaeb5
HughTB <hugh.baldwin77@gmail.com>
2024-03-17 15:50:54 +0000

    potentially fix table formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,10 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Custom CSS paths
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/tables.css',
+]
